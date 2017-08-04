c939cc00ce4a4f688613a710e2c64585c578cd98
liuhongbo <yahooandgoogle@126.com>
2017-08-04 15:13:39 +0800

    update

--- src/main/java/com/ai/redis/util/EncodeUtils.java
+++ src/main/java/com/ai/redis/util/EncodeUtils.java
@@ -7,7 +7,7 @@ import java.net.URLEncoder;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
-import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang3.StringEscapeUtils;
 
 /**
  * 编码加码工具类.
@@ -147,14 +147,14 @@ public class EncodeUtils {
 	 * Html 转码.
 	 */
 	public static String htmlEscape(String html) {
-		return StringEscapeUtils.escapeHtml(html);
+		return StringEscapeUtils.escapeHtml4(html);
 	}
 
 	/**
 	 * Html 解码.
 	 */
 	public static String htmlUnescape(String htmlEscaped) {
-		return StringEscapeUtils.unescapeHtml(htmlEscaped);
+		return StringEscapeUtils.unescapeHtml4(htmlEscaped);
 	}
 
 	/**
--- src/main/java/com/ai/redis/util/JsonUtils.java
+++ src/main/java/com/ai/redis/util/JsonUtils.java
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
