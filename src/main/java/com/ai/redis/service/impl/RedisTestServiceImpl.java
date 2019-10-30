11594bb6b66b2c3b69ec2de41d1058ce2698df7a
liuhongbo <yahooandgoogle@126.com>
2019-10-30 15:47:46 +0800

    update

--- src/main/java/com/ai/redis/service/impl/RedisTestServiceImpl.java
+++ src/main/java/com/ai/redis/service/impl/RedisTestServiceImpl.java
@@ -3,6 +3,11 @@ package com.ai.redis.service.impl;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.ai.redis.service.RedisTestService;
@@ -13,8 +18,17 @@ import com.ai.redis.service.RedisTestService;
  */
 @Service("redisTestService")
 public class RedisTestServiceImpl implements RedisTestService {
+    private static final Logger logger = LoggerFactory.getLogger(RedisTestService.class);
+    @Resource
+    private RedisTemplate<String,String> redisTemplate;
+
     @Override
     public String test(String serviceName) {
+        redisTemplate.opsForValue().set("aa","china");
+        String aa = redisTemplate.opsForValue().get("aa");
+        if (logger.isInfoEnabled()) {
+            logger.info("aa value is {} .",aa);
+        }
         return serviceName.concat(" IS RUNNING! ")
                 .concat(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
     }
